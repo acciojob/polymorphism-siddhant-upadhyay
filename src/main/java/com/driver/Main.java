b84d9252cbebaefff8a31a416c2c2d51fde9e260
KIIT <siddhantupadhyay6@gmail.com>
2023-06-16 14:24:40 +0530

    polymorphism code has been addded

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+
+        p.product( 10 , 5);
+
+        p.product( 4,  15 ,  3);
+
+        p.product( 2.9 ,  5.8);
+    }
+
 }
